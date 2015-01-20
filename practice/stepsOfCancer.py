869cab042a23835ce06a1a7d4398d1e34811aeff
Zafarali Ahmed <zafarali.ahmed@gmail.com>
2015-01-20 17:22:50 -0500

    practicing with python

---
+++ practice/stepsOfCancer.py
@@ -0,0 +1,5 @@
+stepsOfCancer = ['Mutagenesis', 'Growth', 'Detachment (EMT)', 'Intravasation', 'Transport/Migration', 'Extravasation', 'Micrometastasis', 'Cononization', 'Proliferation', 'Angiogenesis', 'Secondary Tumor has been formed']
+for step in stepsOfCancer:
+	print step
+
+print 'done'
