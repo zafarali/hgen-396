d22374cc939d6b7da2736aaa32cb8954cbbeb7a8
Zafarali Ahmed <zafarali.ahmed@gmail.com>
2015-01-22 13:27:50 -0500

    general housekeeping and commenting

--- models/simpleLinear/weakestLink.py
+++ models/simpleLinear/weakestLink.py
@@ -1,6 +1,17 @@
 import sys, csv, datetime
 import numpy.random as random
 startTime = datetime.datetime.now()
+
+'''
+@version 0.0.2
+@author Zafarali Ahmed
+@args Number of cells
+@args Mean Dissociation constant
+@args Standard deviation of Dissociation constant
+@args Number of Observations needed
+@args@optional Output File (default: output.csv)
+'''
+
 print 'You are only strong as your weakest link'
 
 requiredArguments = 6
@@ -32,19 +43,25 @@ except IOError:
 value = random.normal(meanKd, sdKd);
 
 for i in range(0,int(enumerations)):
+	# range over the number of observations you want
 	highestKd = float(meanKd)
 	cellOfHighestKd = 0
+
+	#range over the number of cells in your string
 	for j in range(0, int(numCells)):
 		thisKd = random.normal(float(meanKd), float(sdKd))
+
+		#checking if the new kd is higher than what we already have
 		if thisKd > highestKd:
 			highestKd = thisKd
 			cellOfHighestKd = j
+
+	#write the data collected
 	try:
 		writer = csv.writer(f)
 		sizeA = int(cellOfHighestKd) - 1
 		sizeB = int(numCells) - int(cellOfHighestKd)
 		writer.writerow([highestKd,sizeA,sizeB])
-		print [highestKd,sizeA,sizeB]
 	except IOError:
 		print 'cannot write'
 
