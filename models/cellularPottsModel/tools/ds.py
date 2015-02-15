0ea0e43b6b37abc7dd4295734270c8b6564d9918
zafarali <zafarali.ahmed@gmail.com>
2015-02-15 22:59:21 +0000

    added some tools to make manipulation easier

--- models/cellularPottsModel/tools/ds.py
+++ models/cellularPottsModel/tools/ds.py
@@ -3,6 +3,17 @@ __author__ = 'zafarali'
 
 import numpy as np
 
+class Tools:
+    @staticmethod
+    def rndm( l, r ):
+        return round(np.random.random())*(r-l)
+    @staticmethod
+    def probability( threshold ):
+        if (np.random.random() <= threshold) or threshold is 1:
+            return True
+        else:
+            return False
+
 class Lattice:
     # constants
     DIMENSION = 2
@@ -16,9 +27,9 @@ class Lattice:
     
     def __init__(self, size):
         self.size = size
-        #create the matrix of zeros (essentially a blank lattice)
-        self.matrix = [[0 for i in range(self.size)] for j in range(self.size)]
-        self.matrix = np.array(self.matrix)
+        #create the matrix of zeros (essentially a blank lattice
+        
+        self.matrix = np.zeros((size,size))
     
     def giveName(self, name):
         self.name = name
@@ -27,16 +38,22 @@ class Lattice:
         self.cellArea = cellArea
         self.numberOfCells = numberOfCells
         #code to initialize lattice here
-
+        
+        # we start from the middle of the lattice
+        cellIndex = [self.size/2, self.size/2]
         for i in range(1,numberOfCells+1):
-
             pass
-        pass
+        
     def isPositionOccupied(self, x, y):
         return bool(self.matrix[x][y])
 
-    def setLatticePosition(self, x, y, value):
-        if not isPositionOccupied(x,y):
+    
+    def setLatticePosition(self, x, y, value, override):
+        # Sets a lattice position (x,y) to value = value
+        # returns true if position was not occupied and it was updated
+        # returns false if the position was occupied and it was not updated
+        # override is a boolean that allows us to update even though position is occupied
+        if (override == True) or (not isPositionOccupied(x,y)):
             self.matrx[x][y] = value
             return True
         else:
--- models/cellularPottsModel/tools/test.py
+++ models/cellularPottsModel/tools/test.py
@@ -14,3 +14,7 @@ print 'x.matrix[1][2]=',x.matrix[1][2]
 print 'Reassigning x.matrix[1][2]=1'
 x.matrix[1][2] = 1
 print 'x.matrix[1][2]=',x.matrix
+print str(ds.Tools.rndm(5,10))
+print 'running probability with p=0.25'
+for i in range(1,10):
+    print str(ds.Tools.probability(0.25))
