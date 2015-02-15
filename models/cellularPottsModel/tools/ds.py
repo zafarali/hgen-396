b6190617ef348889e0ac22a18b1f3b7a4f5785ef
zafarali <zafarali.ahmed@gmail.com>
2015-02-15 23:23:44 +0000

    added initialization algorithm

--- models/cellularPottsModel/tools/ds.py
+++ models/cellularPottsModel/tools/ds.py
@@ -6,7 +6,7 @@ import numpy as np
 class Tools:
     @staticmethod
     def rndm( l, r ):
-        return round(np.random.random())*(r-l)
+        return round((r-l)*np.random.random() +l)
     @staticmethod
     def probability( threshold ):
         if (np.random.random() <= threshold) or threshold is 1:
@@ -42,19 +42,40 @@ class Lattice:
         # we start from the middle of the lattice
         cellIndex = [self.size/2, self.size/2]
         for i in range(1,numberOfCells+1):
-            pass
+            
+            if(self.setLatticePosition(cellIndex[0], cellIndex[1], i)):
+                # set everything the the von neumann neighbourhood
+                # to the same cell type
+                self.setLatticePosition(cellIndex[0],cellIndex[1]-1,i)
+                self.setLatticePosition(cellIndex[0],cellIndex[1]+1,i)
+                self.setLatticePosition(cellIndex[0]-1,cellIndex[1],i)
+                self.setLatticePosition(cellIndex[0]-1,cellIndex[1],i)
+                
+                switch = Tools.rndm(1,4)
+                if switch == 1:
+                    self.setLatticePosition(cellIndex[0]+1,cellIndex[1]+1,i)
+                elif switch == 2:
+                    self.setLatticePosition(cellIndex[0]+1,cellIndex[1]-1,i)
+                elif switch == 2:
+                    self.setLatticePosition(cellIndex[0]-1,cellIndex[1]+1,i)
+                else:             
+                    self.setLatticePosition(cellIndex[0]-1,cellIndex[1]-1,i)
+            else:
+                i -= 1
+            cellIndex[0] = (cellIndex[0] + Tools.rndm(0, self.size)) % self.size/2
+            cellIndex[1] = (cellIndex[1] + Tools.rndm(0, self.size)) % self.size/2
         
     def isPositionOccupied(self, x, y):
         return bool(self.matrix[x][y])
 
     
-    def setLatticePosition(self, x, y, value, override):
+    def setLatticePosition(self, x, y, value, override=0):
         # Sets a lattice position (x,y) to value = value
         # returns true if position was not occupied and it was updated
         # returns false if the position was occupied and it was not updated
         # override is a boolean that allows us to update even though position is occupied
-        if (override == True) or (not isPositionOccupied(x,y)):
-            self.matrx[x][y] = value
+        if (override == True) or (not self.isPositionOccupied(x,y)):
+            self.matrix[x][y] = value
             return True
         else:
             return False
