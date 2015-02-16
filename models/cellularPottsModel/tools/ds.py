eff8d3eee2486a5d18dc43cc9a8333e0b324f1ce
zafarali <zafarali.ahmed@gmail.com>
2015-02-16 00:02:59 +0000

    initializing code fix 1

--- models/cellularPottsModel/tools/ds.py
+++ models/cellularPottsModel/tools/ds.py
@@ -42,7 +42,6 @@ class Lattice:
         # we start from the middle of the lattice
         cellIndex = [self.size/2, self.size/2]
         for i in range(1,numberOfCells+1):
-            
             if(self.setLatticePosition(cellIndex[0], cellIndex[1], i)):
                 # set everything the the von neumann neighbourhood
                 # to the same cell type
@@ -51,6 +50,7 @@ class Lattice:
                 self.setLatticePosition(cellIndex[0]-1,cellIndex[1],i)
                 self.setLatticePosition(cellIndex[0]-1,cellIndex[1],i)
                 
+                #update one lattice site outside the von neumann neighbourhood to be the same as the current cell
                 switch = Tools.rndm(1,4)
                 if switch == 1:
                     self.setLatticePosition(cellIndex[0]+1,cellIndex[1]+1,i)
@@ -62,8 +62,20 @@ class Lattice:
                     self.setLatticePosition(cellIndex[0]-1,cellIndex[1]-1,i)
             else:
                 i -= 1
-            cellIndex[0] = (cellIndex[0] + Tools.rndm(0, self.size)) % self.size/2
-            cellIndex[1] = (cellIndex[1] + Tools.rndm(0, self.size)) % self.size/2
+            # update coordinates for next location
+            print i
+            
+            switch = i%4
+            scale = 
+            if switch == 1:
+                cellIndex[1] = cellIndex[1] - 1
+            elif switch == 2:
+                cellIndex[0] = cellIndex[0]+1
+            elif switch == 3:
+                cellIndex[1] = cellIndex[1]+1
+            else:
+                cellIndex[0] = cellIndex[0]-1
+            
         
     def isPositionOccupied(self, x, y):
         return bool(self.matrix[x][y])
@@ -74,11 +86,13 @@ class Lattice:
         # returns true if position was not occupied and it was updated
         # returns false if the position was occupied and it was not updated
         # override is a boolean that allows us to update even though position is occupied
-        if (override == True) or (not self.isPositionOccupied(x,y)):
-            self.matrix[x][y] = value
-            return True
-        else:
-            return False
+        print x,y,self.size
+        if x < self.size and y < self.size:
+            if (override == True) or (not self.isPositionOccupied(x,y)):
+                self.matrix[x][y] = value
+                return True
+            else:
+                return False
 
 class Cell:
     cellType=0
