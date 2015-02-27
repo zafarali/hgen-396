767dc545d71599abeeb647d885882d25af18c589
Zafarali Ahmed <zafarali.ahmed@gmail.com>
2015-02-27 10:18:45 -0500

    adds some plotting code

--- models/cellularPottsModel/tools/test.py
+++ models/cellularPottsModel/tools/test.py
@@ -3,16 +3,30 @@ __author__ = 'zafarali'
 #
 #
 
+## plotting stuff
+# import plotly.plotly as pltly
+# from plotly.graph_objs import *
+# import plotly.tools as pltls
+
 import ds
 
-x = ds.Lattice(10)
+x = ds.Lattice(20)
 print 'x.size=',x.size
 #initialize with 1 cell
-x.initialize(1)
+
+x.initialize(10)
 print 'x.matrix=',x.matrix
+
+# prior = Heatmap(z=x.matrix.tolist())
+
 print 'running simulation for 3 MCS'
-x.runSimulation(3)
+x.runSimulation(20)
 print 'x.matrix=',x.matrix
+
+
+# prosterior = Heatmap(z=x.matrix.tolist())
+
+
 #print str(ds.Tools.rndm(5,10))
 #print 'running probability with p=0.25'
 #for i in range(1,10):
@@ -20,6 +34,13 @@ print 'x.matrix=',x.matrix
 #print 'running probability with p=0.9'
 #for i in range(1,10):
 #    print str(ds.Tools.probability(0.9))
+
+# data = Data([prior])
+# fig = Figure(data=data)
+# data2 = Data([prosterior])
+# fig2 = Figure(data=data2)
+# plot_url = pltly.plot(fig, filename='simulation-before')
+# plot_url2 = pltly.plot(fig2, filename='simulation-after')
 
 
 #testing for cells.
@@ -32,3 +53,4 @@ print str(x.interactionStrength(cell1,cell3))
 print str(x.interactionStrength(cell3,cell1))
 print 'this should be different'
 print str(x.interactionStrength(cell1,cell2))
+
