adds mutation, fixes metropolis H calculation, adds check if all neighbours are of all same cell type and to end

models/cellularPottsModel/tools/Tools.py
@@ -31,6 +31,6 @@ class Tools:
             return 0
         else:
             return 1
-    # @staticmethod
-    # def poissonProbability(lamb):
-    #     return np.random.possion(lamb)
+    @staticmethod
+    def poissonProbability(lamb):
+        return np.random.poisson(lamb)