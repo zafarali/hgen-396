95209e74ef396067da3203ba0f6ab1d2dad9a769
Zafarali Ahmed <zafarali.ahmed@gmail.com>
2015-03-15 20:16:20 -0400

    removes the concept of attractive vs repulsive gradients, seemed to make the implementation more confusing.

--- models/cellularPottsModel/tools/Gradient.py
+++ models/cellularPottsModel/tools/Gradient.py
@@ -3,31 +3,27 @@ import numpy as np
 class Gradient:
 	# creates a 1D gradient
 
-	# interaction type: -1 for attractive (less energy to go in that direction)
-	# interaction type: +1 for repulsive (more energy to go in that direction)
-	def __init__( self, gradientFunc, latticeSize, interactionType = -1, interactionStrength = 0):
+
+	def __init__( self, gradientFunc, latticeSize, interactionStrength = 0):
 		
 		self.interactions = [gradientFunc(i) for i in range(0, latticeSize)]
 		# print self.interactions
 		self.interactionStrength = interactionStrength
-		self.interactionType = interactionType
 
 	def interactionAt ( self, pos ):
 		try:
-			return self.interactions[pos] * self.interactionStrength * self.interactionType
+			return self.interactions[pos] * self.interactionStrength
 		except IndexError:
 			return 0
 
 	def __repr__(self):
 		string = '[ %s ]' % str(self.interactions)
 		string += '\n %d ' % self.interactionStrength
-		string += 'attractive' if self.interactionType == -1 else 'repulsive'
 
 		return string	
 	def __str__(self):
 		string = ' %s ' % str(self.interactions)
 		string += '\n %d ' % self.interactionStrength
-		string += 'attractive' if self.interactionType == -1 else 'repulsive'
 		return string
 
 '''end'''
