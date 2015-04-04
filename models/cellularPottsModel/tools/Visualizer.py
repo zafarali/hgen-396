a308601c0d2845e8791027f7b795ff5dd269e100
Zafarali Ahmed <zafarali.ahmed@gmail.com>
2015-04-04 11:16:06 -0400

    Rewrite to handle multiple visualizations at once

--- models/cellularPottsModel/tools/Visualizer.py
+++ models/cellularPottsModel/tools/Visualizer.py
@@ -3,30 +3,31 @@ import csv
 import numpy as np
 import matplotlib.pyplot as plt
 
+def Visualizer( data, N, name ):
+	# NEW COLOR BAR
+	def discrete_cmap(N, base_cmap='jet'):
+		if N == 1:
+			return plt.cm.get_cmap('jet')
+		base = plt.cm.get_cmap(base_cmap)
+		color_list = base(np.linspace(0,1,N+1))
+		# print color_list
+		cmap_name = base.name+str(N)
+		return base.from_list(cmap_name, color_list, N+1)
 
-fileName = str(sys.argv[1])
+	plt.figure('Visualization of '+name)
+	plt.imshow(data, interpolation='nearest', cmap=discrete_cmap(N))
+	plt.colorbar(orientation='vertical', ticks=range(N+1))
+	plt.draw()
+	plt.show()
+	print 'Complete'
 
-
-data = []
-with open(fileName, 'rb') as csvfile:
-	reader = csv.reader(csvfile)
-	N = int(reader.next()[0])
-	for row in reader:
-		data.append(map(float, row))
-
-# NEW COLOR BAR
-def discrete_cmap(N, base_cmap='jet'):
-	if N == 1:
-		return plt.cm.get_cmap('jet')
-	base = plt.cm.get_cmap(base_cmap)
-	color_list = base(np.linspace(0,1,N+1))
-	# print color_list
-	cmap_name = base.name+str(N)
-	return base.from_list(cmap_name, color_list, N+1)
-
-plt.figure('Visualization of '+fileName)
-plt.imshow(data, interpolation='nearest', cmap=discrete_cmap(N))
-plt.colorbar(orientation='vertical', ticks=range(N+1))
-plt.draw()
-plt.show()
-print 'Complete'
+if __name__ == '__main__':
+	fileNames = sys.argv[1:]
+	for fileName in fileNames:
+		data = []
+		with open(fileName, 'rb') as csvfile:
+			reader = csv.reader(csvfile)
+			N = int(reader.next()[0])
+			for row in reader:
+				data.append(map(float, row))
+		Visualizer(data, N, fileName)
