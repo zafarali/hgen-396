e14148db0800c3d9718554f2fd0d75822539c954
Zafarali Ahmed <zafarali.ahmed@gmail.com>
2015-04-05 03:20:51 -0400

    Fix #28, we can now accurately calculate components

--- models/cellularPottsModel/tools/Components.py
+++ models/cellularPottsModel/tools/Components.py
@@ -8,9 +8,10 @@ from Tools import Tools
 def numberOfComponents( data, N ):
 	# create a matrix of zeros
 	# to store the graph
-	G = np.zeros((N,N))
 
 	isEdgeCase = lambda x,y: Tools.isEdgeCase(x, y, 0, len(data))
+
+	edgeList = []
 
 	# loop through all the lattice positions
 	for x in range( len( data ) ):
@@ -25,16 +26,34 @@ def numberOfComponents( data, N ):
 				#check if this neighbour is an edge case
 				if not isEdgeCase( nx, ny ):
 					# print 'not edge case'
-					if data[x][y] != 0 and data[nx][ny] != 0 :
-						print data[x][y], data[nx][ny]
 					# if the spins in each position are different and non zero
-					if ( data[x][y] != data[nx][ny] ) and ( ( data[x][y] != 0 ) and ( data[nx][ny] != 0 ) ):
+					# if ( data[x][y] != data[nx][ny] ) and ( ( data[x][y] != 0 ) and ( data[nx][ny] != 0 ) ):
+					if ( data[x][y] != data[nx][ny] ):
 						# obtain the 'spin'/'node' 
 						# convert it to an index for the graph G.
-						node1 = int(data[x][y]) - 1
-						node2 = int(data[nx][ny]) - 1
-						G[node1][node2] = 1
-						G[node2][node1] = 1
+						node1 = int(data[x][y])
+						node2 = int(data[nx][ny])
+						edgeList.append( ( node1, node2 ) )
+						edgeList.append( ( node2, node1 ) )
+
+	import itertools
+
+	# create a list of nodes from the edge list
+	nodeList = list( set( list( itertools.chain( *edgeList ) ) ) )
+	# number of non-zero nodes
+	nbNodes = len( nodeList ) - 1
+	G = np.zeros( ( nbNodes , nbNodes) )
+
+	for e in edgeList:
+		u,v = e
+		# if both incoming and outgoing are not-zero we populate
+		# G with 1s, else we don't
+		if u != 0 and v != 0 :
+			i = nodeList.index(u)-1
+			j = nodeList.index(v)-1
+			G[i][j] = 1
+			G[j][i] = 1
+
 	print G
 	# create a sparse graph
 	graph = sparse.csr_matrix(G)
