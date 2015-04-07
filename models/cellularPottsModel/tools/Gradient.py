Add 2D gradient and fix for boundary conditions in 1D gradient

models/cellularPottsModel/tools/Gradient.py
@@ -14,9 +14,9 @@ class Gradient:
 			return self.interactions[pos] * self.interactionStrength
 		except IndexError:
 			if pos < 0:
-				return self.interactions[0]
-			elif pos > len(self.interactions):
-				return self.interactions[len(self.interactions)]
+				return self.interactions[0] * self.interactionStrength
+			elif pos >= len(self.interactions):
+				return self.interactions[len(self.interactions)-1] * self.interactionStrength
 			else:
 				return 0
 
@@ -29,5 +29,33 @@ class Gradient:
 		string = ' %s ' % str(self.interactions)
 		string += '\n %d ' % self.interactionStrength
 		return string
+
+
+class Gradient2D(Gradient):
+	# creates a 2D gradient
+
+	def __init__( self, gradientFunc, latticeSize, interactionStrength = 1 ):
+		# generate a surface for the potential
+		z = [ [ gradientFunc( x, y ) for x in range( 0, latticeSize ) ] for y in range( 0 , latticeSize ) ]
+		self.interactions = np.array(z)
+
+		self.interactionStrength = interactionStrength
+
+	def interactionAt ( self, x, y ):
+		M = len(self.interactions)
+		try:
+			return self.interactions[x][y] * self.interactionStrength
+		except IndexError:
+			# we are possibly in an edge case
+			if x < 0 and y < 0:
+				return self.interactions[0][0] * self.interactionStrength
+			elif x >= M and y >= M:
+				return self.interactions[M-1][M-1] * self.interactionStrength
+			elif x >= M and y < 0:
+				return self.interactions[0][M-1] * self.interactionStrength
+			elif y >= M and x < 0:
+				return self.interactions[M-1][0] * self.interactionStrength
+			else:
+				return 0
 
 '''end'''